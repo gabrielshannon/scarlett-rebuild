4f55657d77723faf1c721b5f6e09f280ba6b95d8
gabrielshannonsocius <113363825+gabrielshannonsocius@users.noreply.github.com>
2023-01-19 19:16:19 +0000

    added info pages and logo

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const about = () => {
-  return (
-    <section className="about">
+const contact= () => {
+    return (
+        <div className="gallery_section">
       <div className="top-section">
         <div className="bio_section">
           <div className="bio-paragraph">
@@ -15,26 +15,61 @@ const about = () => {
      
           </div>
         </div>
-
-     
-      </div>
-      <navbar class="navbar-about">
-        <div class="title_option">
-          <a href="/">HOME</a>,
         </div>
-        <div class="title_option">
-          <a href="/works">WORKS</a>,
+    
+          <navbar class="navbar_info">
+            <div class="title_option">
+              <a href="/">HOME</a>,
+            </div>
+            <div class="title_option">
+              <a href="/works">WORKS</a>,
+            </div>
+            <div class="title_option">
+              <a href="/about">ABOUT</a>,
+            </div>
+    
+            <div class="title_option">
+              <a href="/contact">CONTACT</a>
+            </div>
+          </navbar>
         </div>
-        <div class="title_option">
-          <a href="/about">ABOUT</a>,
-        </div>
-
-        <div class="title_option">
-          <a href="">CONTACT</a>
-        </div>
-      </navbar>
-    </section>
-  );
+      );
 };
 
-export default about;
+export default contact;
+
+
+// return (
+//     <section className="about">
+    //   <div className="top-section">
+    //     <div className="bio_section">
+    //       <div className="bio-paragraph">
+    //         <h1 style={{marginBottom:20}}>INSTAGRAM @SQUALOPRODUZIONI</h1>
+    //         <h1 style={{marginBottom:20}}>EMAIL INFO@SQUALOPRODUZIONI.COM</h1>
+    //         <h1 style={{marginBottom:20}}>+39 349 68 58 748</h1>
+    //         <h1 style={{marginBottom:20}}>WEB DESIGN GIULIA NARDI </h1>
+    //         <h1 style={{marginBottom:20}}> WEB DEVELOPMENT GIULIO LIBERATI</h1>
+    //         <h1>LOGO DESIGN FABIO DEMITRI</h1>
+     
+    //       </div>
+    //     </div>
+
+     
+//       </div>
+//       <navbar class="navbar-about">
+//         <div class="title_option">
+//           <a href="/">HOME</a>,
+//         </div>
+//         <div class="title_option">
+//           <a href="/works">WORKS</a>,
+//         </div>
+//         <div class="title_option">
+//           <a href="/about">ABOUT</a>,
+//         </div>
+
+//         <div class="title_option">
+//           <a href="">CONTACT</a>
+//         </div>
+//       </navbar>
+//     </section>
+//   );
